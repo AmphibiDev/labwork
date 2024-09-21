8a4d375bdb0c17be97b08f64020ab667cdd176cd
AmphibiDev <AmphibiDev@>
2024-09-21 23:12:43 +0300

    Updated code

--- lab2_02.cpp
+++ lab2_02.cpp
@@ -1,38 +1,28 @@
 ﻿#include <iostream>
-#include <windows.h>
 #include <cmath>
 
 #define CHECK_TYPE true	// Перевірка через switch (True) / через if-else (False)
 
 int main()
 {
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-
 	float x, y, fee; char type;
 	std::cout << "Enter \"x\": "; std::cin >> x;
 	std::cout << "Enter work type: "; std::cin >> type;
-	
+
 	#ifdef CHECK_TYPE
 	// Using Switch
-	switch (type){
-		case 'А': { 
-			y = 100 * fabs(sin(pow(x, 2)) * cos(pow(x, 3)) - sin(x) + 5.2f + 50); fee = 0.1; break;
-		}
-		case 'Б': {
-			y = 150 * fabs(2 * sin(x) * sin(2 * x - 1.5f) * cos(2 * x - 1.5f) - 6 + 100); fee = 0.15; break;
-		}
-		case 'В': {
-			y = 100 * fabs(fabs(cos(pow(x, 2)) - 0.51f) * sin(3 * x - 4) - 4.44f + 135); fee = 0.2; break;
-		}
-		default: std::cout << type << "Wrong work type" << std::endl; break;
+	switch (type) {
+	case 'А': { y = 100 * fabs(sin(pow(x, 2)) * cos(pow(x, 3)) - sin(x) + 5.2f + 50); fee = 0.1; break; }
+	case 'Б': { y = 150 * fabs(2 * sin(x) * sin(2 * x - 1.5f) * cos(2 * x - 1.5f) - 6 + 100); fee = 0.15; break; }
+	case 'В': { y = 100 * fabs(fabs(cos(pow(x, 2)) - 0.51f) * sin(3 * x - 4) - 4.44f + 135); fee = 0.2; break; }
+	default: std::cout << "Wrong work type" << std::endl; break;
 	}
 	#else
 	// Using If-Else
-	if (type == 'А') y = 100 * fabs(sin(pow(x, 2)) * cos(pow(x, 3)) - sin(x) + 5.2f + 50);
-	else if (type == 'Б') y = 150 * fabs(2 * sin(x) * sin(2 * x - 1.5f) * cos(2 * x - 1.5f) - 6 + 100);
-	else if (type == 'В') y = 100 * fabs(fabs(cos(pow(x, 2)) - 0.51f) * sin(3 * x - 4) - 4.44f + 135);
-	else std::cout << type << "Wrong work type" << std::endl;
+	if (type == 'А') { y = 100 * fabs(sin(pow(x, 2)) * cos(pow(x, 3)) - sin(x) + 5.2f + 50); fee = 0.1f; }
+	else if (type == 'Б') { y = 150 * fabs(2 * sin(x) * sin(2 * x - 1.5f) * cos(2 * x - 1.5f) - 6 + 100); fee = 0.15f; }
+	else if (type == 'В') { y = 100 * fabs(fabs(cos(pow(x, 2)) - 0.51f) * sin(3 * x - 4) - 4.44f + 135); fee = 0.2f; }
+	else std::cout << "Wrong work type" << std::endl;
 	#endif
 
 	std::cout << "Amount Charged = " << y << std::endl;			// Нарахована сума
--- lab2_03.cpp
+++ lab2_03.cpp
@@ -1,38 +1,44 @@
 ﻿#include <iostream>
 #include <windows.h>
-#include <unordered_map>
 
 struct city_info {
 	int population;
 	float area;
 
 	city_info() = default;
+	~city_info() = default;
 	city_info(int population, float area) {
 		this->population = population;
 		this->area = area;
 	}
 };
 
+std::ostream& operator<< (std::ostream& stream, const city_info& data) {
+	return stream << "Population: " << data.population << " | Area: " << data.area << std::endl;
+}
+
 int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	std::unordered_map<char, city_info> cities_data = {
-		{'А', city_info(37210, 29.00f)}, // avdeevka
-		{'Б', city_info(82916, 41.60f)}, // bahmut
-		{'В', city_info(356665, 113.20f)}, // vinnitsya
-		{'Г', city_info(292250, 422.00f)}, // gorlivka
-		{'Д', city_info(1065008, 415.00f)}, // dnipro
-		{'Е', city_info(56242, 63.50f)}, // energodar
-
-	};
-
 	char letter;
-	std::cin >> letter;
+	std::cout << "Enter city first letter: "; std::cin >> letter;
 
-	if (cities_data.find(letter) == cities_data.end()) { std::cout << "Haven't found!"; return 1; }
-	std::cout << "Population: " << cities_data[letter].population << "\nArea: " << cities_data[letter].area << std::endl;
+	switch (letter) {
+		case 'А': std::cout << city_info(37210, 29.00f); break; // avdeevka
+		case 'Б': std::cout << city_info(82916, 41.60f); break; // bahmut
+		case 'В': std::cout << city_info(356665, 113.20f); break; // vinnitsya
+		case 'Г': std::cout << city_info(292250, 422.00f); break; // gorlivka
+		case 'Д': std::cout << city_info(1065008, 415.00f); break; // dnipro
+		case 'Е': std::cout << city_info(56242,	63.50f); break; // energodar
+		case 'Є': std::cout << city_info(105915, 222.70f); break; // yevpatoriya
+		case 'Ж': std::cout << city_info(284236, 61.50f); break; // zhitomyr
+		case 'З': std::cout << city_info(815256, 331.50f); break; // energodar
+		case 'І': std::cout << city_info(218359, 83.70f); break; // ivano-frankivsk
+		case 'К': std::cout << city_info(255841, 138.00f); break; // kamyanske
+		default: std::cout << "Haven't found!"; break;
+	}
 
 	return 0;
 }
