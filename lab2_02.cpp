5cbd74b50287fd259edd4f1c0df2a64bfe19b1ae
AmphibiDev <AmphibiDev@>
2024-09-21 19:02:43 +0300

    Updated code

--- lab2_02.cpp
+++ lab2_02.cpp
@@ -2,29 +2,42 @@
 #include <windows.h>
 #include <cmath>
 
+#define CHECK_TYPE true	// Перевірка через switch (True) / через if-else (False)
+
 int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	float x; char type;
+	float x, y, fee; char type;
 	std::cout << "Enter \"x\": "; std::cin >> x;
-	
 	std::cout << "Enter work type: "; std::cin >> type;
+	
+	#ifdef CHECK_TYPE
+	// Using Switch
 	switch (type){
 		case 'А': { 
-			// sin(pow(x, 2))* cos(pow(x, 3)) - sin(x) + 5.2f
-			std::cout << "y = " << 100 * abs(sin(pow(x, 2)) * cos(pow(x, 3)) - sin(x) + 5.2f + 50); break;
+			y = 100 * fabs(sin(pow(x, 2)) * cos(pow(x, 3)) - sin(x) + 5.2f + 50); fee = 0.1; break;
 		}
 		case 'Б': {
-			// 2*sin(x)*sin(2*x-1.5f)*cos(2*x-1.5f)-6
-			std::cout << "y = " << 150 * abs(2 * sin(x) * sin(2 * x - 1.5f) * cos(2 * x - 1.5f) - 6 + 100); break;
+			y = 150 * fabs(2 * sin(x) * sin(2 * x - 1.5f) * cos(2 * x - 1.5f) - 6 + 100); fee = 0.15; break;
 		}
 		case 'В': {
-			// abs(cos(pow(x, 2)) - 0.51f) * sin(3*x-4)-4.44f
-			std::cout << "y = " << 100 * abs(abs(cos(pow(x, 2)) - 0.51f) * sin(3 * x - 4) - 4.44f + 135); break;
+			y = 100 * fabs(fabs(cos(pow(x, 2)) - 0.51f) * sin(3 * x - 4) - 4.44f + 135); fee = 0.2; break;
 		}
-		default: std::cout << type << "Wrong type" << std::endl; break;
+		default: std::cout << type << "Wrong work type" << std::endl; break;
 	}
+	#else
+	// Using If-Else
+	if (type == 'А') y = 100 * fabs(sin(pow(x, 2)) * cos(pow(x, 3)) - sin(x) + 5.2f + 50);
+	else if (type == 'Б') y = 150 * fabs(2 * sin(x) * sin(2 * x - 1.5f) * cos(2 * x - 1.5f) - 6 + 100);
+	else if (type == 'В') y = 100 * fabs(fabs(cos(pow(x, 2)) - 0.51f) * sin(3 * x - 4) - 4.44f + 135);
+	else std::cout << type << "Wrong work type" << std::endl;
+	#endif
+
+	std::cout << "Amount Charged = " << y << std::endl;			// Нарахована сума
+	std::cout << "Tax Amount = " << y * fee << std::endl;		// Сума податку
+	std::cout << "Total Sum = " << y * (1 - fee) << std::endl;	// Сума до видачі
+
 	return 0;
 }
