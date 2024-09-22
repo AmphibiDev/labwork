72c1f6463506fd6136e0985130bc7df8bf968aa8
AmphibiDev <AmphibiDev@>
2024-09-22 13:40:19 +0300

    Updated script

--- lab2_03.cpp
+++ lab2_03.cpp
@@ -37,6 +37,21 @@ int main()
 		case 'З': std::cout << city_info(815256, 331.50f); break; // energodar
 		case 'І': std::cout << city_info(218359, 83.70f); break; // ivano-frankivsk
 		case 'К': std::cout << city_info(255841, 138.00f); break; // kamyanske
+		case 'Л': std::cout << city_info(115229, 95.64f); break; // lysychansk
+		case 'М': std::cout << city_info(492176, 134.70f); break; // mariupol
+		case 'Н': std::cout << city_info(136280, 50.00f); break; // nikopol
+		case 'О': std::cout << city_info(1029049, 162.42f); break; // odesa
+		case 'П': std::cout << city_info(317998, 103.00f); break; // poltava
+		case 'Р': std::cout << city_info(248813, 63.00f); break; // rivne
+		case 'С': std::cout << city_info(248813, 864.00f); break; // sevastopol
+		case 'Т': std::cout << city_info(227755, 86.00f); break; // ternopil
+		case 'У': std::cout << city_info(117317, 65.00f); break; // uzhgorod
+		case 'Х': std::cout << city_info(1470902, 350.00f); break; // kharkiv
+		case 'Ч': std::cout << city_info(304994, 79.00f); break; // chernihiv	
+		case 'Ш': std::cout << city_info(59589, 19.69f); break; // shahtarsk	
+		case 'Щ': std::cout << city_info(11699, 3.42f); break; // shcholkine	
+		case 'Ю': std::cout << city_info(38206, 24.38f); break; // yuzhnoukrainsk	
+		case 'Я': std::cout << city_info(81654, 28.29f); break; // yalta	
 		default: std::cout << "Haven't found!"; break;
 	}
 
